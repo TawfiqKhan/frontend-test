185b892bed8d75bcd08ccc880ce3f50d8682e7ea
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-09-04 02:25:21 -0400

    display product description on hover, add css

--- src/Components/Products/ProductList.tsx
+++ src/Components/Products/ProductList.tsx
@@ -2,16 +2,26 @@ import "./ProductList.scss";
 import ProductListItem from "./ProductListItem";
 
 interface Product {
-  category: string;
-  percentage: number;
+  _id: string;
+  itemName: string;
+  itemLink: string;
+  vendorName: string;
+  MSRP: number;
 }
 type Props = {
   allProducts: Array<Product>;
 };
 
 const ProductList = ({ allProducts }: Props): JSX.Element => {
-  const productsTodisplay = allProducts.map((product: any) => (
-    <ProductListItem key={product._id} name={product.itemName} />
+  console.log(allProducts);
+  const productsTodisplay = allProducts.map((product) => (
+    <ProductListItem
+      key={product._id}
+      title={product.itemName}
+      image={product.itemLink}
+      brand={product.vendorName}
+      price={product.MSRP}
+    />
   ));
   return <section className="productsContainer">{productsTodisplay}</section>;
 };
--- src/Components/Products/ProductListItem.tsx
+++ src/Components/Products/ProductListItem.tsx
@@ -1,11 +1,29 @@
-type Props = {
-  name: string;
-};
+interface Props {
+  title: string;
+  image: string;
+  brand: string;
+  price: number;
+}
 
-const ProductListItem = ({ name }: Props): JSX.Element => {
+const ProductListItem: React.FC<Props> = ({
+  title,
+  image,
+  brand,
+  price,
+}): JSX.Element => {
   return (
     <div className="product">
-      <h4>{name}</h4>
+      <img className="product-img" src={image} alt="" />
+      <div className="product-desc">
+        <div>
+          <h3>{title.toLowerCase()}</h3>
+          <span>{brand}</span>
+        </div>
+        <div>
+          <button>+Add to Cart</button>
+          <span>${price}</span>
+        </div>
+      </div>
     </div>
   );
 };
